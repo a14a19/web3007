c945ba8d193cd25a1202080b14f6ae327c16b072
a14a19 <anas31197@gmail.com>
2023-12-05 18:47:13 +0530

    new code

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -3,7 +3,7 @@ import { BsArrowRight } from 'react-icons/bs';
 
 function OurWork() {
     return (
-        <main className='ourWorkMain pt-5 px-5'>
+        <main className='ourWorkMain p-5 mb-5'>
             <h2 className='ourWorkHeader'>
                 Our Work
             </h2>
@@ -32,7 +32,7 @@ function OurWork() {
                 </div>
                 <div className='w-full'>
                     <div className='assecoImgContainer'>
-                        <img src="https://decode.agency/wp-content/webp-express/webp-images/uploads/2021/09/asseco-smaller-new.jpg.webp" alt="" className='w-full assecoImg'/>
+                        <img src="https://decode.agency/wp-content/webp-express/webp-images/uploads/2021/09/asseco-smaller-new.jpg.webp" alt="" className='w-full assecoImg' />
                     </div>
                     <div>
                         <p className='ourWorkSubHeading mt-3'>
@@ -43,6 +43,44 @@ function OurWork() {
                         </p>
                         <button className='text-nowrap border-0 ourWorkMain trustedCompanyHeader p-0'>Explore our projects <BsArrowRight /></button>
                     </div>
+                </div>
+            </div>
+            <div className='d-flex justify-content-between align-items-center overflow-hidden marginTopOurWork'>
+                <div className='w-full'>
+                    <div className='assecoImgContainer'>
+                        <img src="https://decode.agency/wp-content/webp-express/webp-images/uploads/2021/10/royal-image-700x700.jpg.webp" alt="" className='w-full assecoImg' />
+                    </div>
+                    <div>
+                        <p className='ourWorkSubHeading mt-3'>
+                            Royal Caribbean
+                        </p>
+                        <p>
+                            Communication and collaboration app
+                        </p>
+                        <button className='text-nowrap border-0 ourWorkMain trustedCompanyHeader p-0'>View case study <BsArrowRight /></button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://decode.agency/wp-content/webp-express/webp-images/uploads/2023/10/cafu-thumbnail-700x564.jpg.webp" alt="" />
+                    </div>
+                    <div>
+                        <p className='ourWorkSubHeading mt-3'>
+                            CAFU
+                        </p>
+                        <p>
+                            All-in-one car service
+                        </p>
+                        <button className='text-nowrap border-0 ourWorkMain trustedCompanyHeader p-0'>View case study <BsArrowRight /></button>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5'>
+                <div className='ourWorkLastHeading'>
+                    <p>
+                        Learn how we combined our expertise to launch our own spin-off software startup —
+                    </p>
+                    <button className='text-nowrap border-0 ourWorkMain trustedCompanyHeader p-0 ourWorkLastHeading'>View Shake <BsArrowRight /></button>
                 </div>
             </div>
         </main>
