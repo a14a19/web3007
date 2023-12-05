96313c8cd00c2588a73f7e25cb5dd226f393129d
a14a19 <anas31197@gmail.com>
2023-12-05 18:08:50 +0530

    anas code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HeroBanner from "./components/HeroBanner";
+import OurWork from "./components/OurWork";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroBanner />
+      <OurWork />
       <Footer />
     </>
   );
